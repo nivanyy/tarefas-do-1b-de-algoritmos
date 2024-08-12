4de88f5e48c6434a6b0b53ed418e71b51f30a1a3
nivanyy <silva.nivany@escolar.ifrn.edu.br>
2024-08-12 11:47:40 -0300

    tarefas algoritmos

---
+++ nivany linda/Program.cs
@@ -0,0 +1,111 @@
+﻿/* string colega1 = " ilolo";
+string colega2 = "ninny";
+string colega3 = " soraya";
+
+int nota1 = 10;
+int nota2 = 900;
+int nota3 = 89;
+Console.WriteLine($" {colega1} tirou {nota1}  e {colega2}  {nota2} e {colega3} {nota3}");
+
+*/
+
+/* int a = 45;
+int b = 67;
+int c;
+
+c = a + b;
+Console.WriteLine("soma:" + c);
+c = a - b;
+Console.WriteLine("subtração:" + c);
+c = a * b;
+Console.WriteLine("  multiplicação:" + c);
+c = a / b;
+Console.WriteLine("divisão:"+ c);
+
+*/
+
+/* int a = 89;
+int b = 9;
+int quociente = a/b;
+int resto = a % b;
+
+Console.WriteLine($"{a} dividido por {b}");
+
+Console.WriteLine("quociente: " + quociente);
+
+Console.WriteLine(" resto:" + resto);
+
+*/
+
+ /* float a = 20;
+float b = 7;
+float quociente = a / b;
+
+Console.WriteLine($"{a} dividido por {b}");
+Console.WriteLine ("quociente:" + quociente);
+Console.WriteLine("resto:"  + resto );
+*/
+
+
+
+
+/*var a = 78;
+var b = 2.567f;
+var c = 9009090909090;
+
+var d = a + b + c;
+
+string mensagem1 = "Resultado da soma";
+char caractere= 'd';
+
+var mensagem2 = mensagem1 + caractere   + "é igual a:";
+
+
+Console.WriteLine(mensagem1 + d);
+
+*/
+
+/*var x = 5;
+var y = 2;
+
+var z = x / y;
+
+Console.WriteLine (z);
+
+*/
+
+/*bool p = true;
+bool q = false;
+
+Console.WriteLine(!p);
+Console.WriteLine(!q);
+Console.WriteLine(p && q);
+Console.WriteLine(p || q);
+*/
+
+
+
+bool p = 7 > 5;
+bool q = 10/2 == 5;
+
+Console.WriteLine (p);
+Console.WriteLine (q);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* string colega1 = " ilolo";
